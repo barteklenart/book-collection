25e9af9abdf4dfadaa3e8b9ef050f29f3e5eb269
barteklenart <bartlomiejlenartbl@gmail.com>
2018-01-13 16:41:14 +0100

    init

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const appDomEl = document.getElementById('app');
+
+const app = (
+    <div>
+        
+    </div>
+);
+
+ReactDOM.render(app, appDomEl);
